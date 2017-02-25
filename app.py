294cd650b1715a35f2604fdff5aa2548652865ce
m-mizen <mark.c.mizen@gmail.com>
2017-02-25 19:58:09 +0000

    updates

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
+
+# Create app object
 app = Flask(__name__)
 
+# Create index route
 @app.route('/')
-
+# Index render function
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
+# Mysterious thing that makes it so other computers can view the server
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
